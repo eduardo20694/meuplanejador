959af77dc325745d91f6adf4ad17d3f7e62353c8
Eduardo Rodrigues <eduardorodrigues@gmail.com>
2025-07-30 17:57:26 -0300

    Att do main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "./App.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Settings from "./pages/Settings.jsx";
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
+import Login from "./pages/login.jsx";
+import Register from "./pages/register.jsx";
 import "./styles/global.css";
 
 function PrivateRoute({ children }) {
